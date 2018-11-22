a98266bf7c0fc63c5fcb909438a623ea8676855e
Alberto Exposito <alberexgar@hotmail.com>
2018-11-22 13:10:14 +0100

    sourcetree

--- hola_mundo.java
+++ hola_mundo.java
@@ -4,7 +4,9 @@
 
 
 public class HolaMundo{
-Scanner sc=new Scanner(System.in)
+Scanner sc=new Scanner(System.in);
+
+//SOURCETREE
 	public static void main(String[] args){
 	System.out.println("HOLA MUNDO");
 String alberto;
